9b76862e66ff409f08620601277a1d83c4f836d3
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-01-01 22:53:46 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: "jit",
+	content: [
+		"./pages/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}"
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")]
+};
